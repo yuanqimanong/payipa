8f2489c2e1662aae98bb5d04d4891f48558ddda3
元气码农少女酱 <1276162429@qq.com>
2026-01-27 15:28:41 +0800

    update

--- app/main.py
+++ app/main.py
@@ -20,4 +20,4 @@ app = FastAPI(
 app.include_router(api_router, prefix=settings.API_VERSION_STR)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=80)
