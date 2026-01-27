7b001168adeeb9a6d818571801270598c949bb46
元气码农少女酱 <1276162429@qq.com>
2026-01-27 18:45:05 +0800

    update

--- app/api/routes/pages.py
+++ app/api/routes/pages.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(tags=["pages"])
@@ -9,6 +9,11 @@ def get_templates():
     return Jinja2Templates(directory="templates")
 
 
-@router.get("/", response_class=HTMLResponse)
+@router.get("/")
+async def root_redirect():
+    return RedirectResponse(url="/index", status_code=301)
+
+
+@router.get("/index", response_class=HTMLResponse)
 async def home_page(request: Request, templates: Jinja2Templates = Depends(get_templates)):
     return templates.TemplateResponse("index.html", {"request": request, "message": "欢迎来到首页"})
