c0468f23b3e37aa6c7202fb8cbd8709c199cfbb5
元气码农少女酱 <1276162429@qq.com>
2026-02-09 11:21:07 +0800

    update

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,11 +1,14 @@
+import uuid
 from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import col, delete, func, select
 
-from app.api.deps import SessionDep, get_current_active_superuser
+from app.api.deps import CurrentUser, SessionDep, get_current_active_superuser
+from app.core.security import get_password_hash, verify_password
 from app.cruds import user_crud
-from app.models.user_model import User, UserCreate, UserPublic, UsersPublic
+from app.models.basic_model import Message
+from app.models.user_model import UpdatePassword, User, UserCreate, UserPublic, UsersPublic, UserUpdate, UserUpdateMe
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -40,3 +43,111 @@ def create_user(*, session: SessionDep, user_in: UserCreate) -> Any:
 
     user = user_crud.create_user(session=session, user_create=user_in)
     return user
+
+
+@router.patch("/me", response_model=UserPublic)
+def update_user_me(*, session: SessionDep, user_in: UserUpdateMe, current_user: CurrentUser) -> Any:
+    """
+    Update own user.
+    """
+
+    if user_in.email:
+        existing_user = user_crud.get_user_by_email(session=session, email=user_in.email)
+        if existing_user and existing_user.id != current_user.id:
+            raise HTTPException(status_code=409, detail="该邮箱的用户已存在")
+    user_data = user_in.model_dump(exclude_unset=True)
+    current_user.sqlmodel_update(user_data)
+    session.add(current_user)
+    session.commit()
+    session.refresh(current_user)
+    return current_user
+
+
+@router.patch("/me/password", response_model=Message)
+def update_password_me(*, session: SessionDep, body: UpdatePassword, current_user: CurrentUser) -> Any:
+    """
+    Update own password.
+    """
+    verified, _ = verify_password(body.current_password, current_user.hashed_password)
+    if not verified:
+        raise HTTPException(status_code=400, detail="密码错误")
+    if body.current_password == body.new_password:
+        raise HTTPException(status_code=400, detail="新密码不能与当前密码相同")
+    hashed_password = get_password_hash(body.new_password)
+    current_user.hashed_password = hashed_password
+    session.add(current_user)
+    session.commit()
+    return Message(message="密码更新成功")
+
+
+@router.get("/me", response_model=UserPublic)
+def read_user_me(current_user: CurrentUser) -> Any:
+    """
+    Get current user.
+    """
+    return current_user
+
+
+@router.get("/{user_id}", response_model=UserPublic)
+def read_user_by_id(user_id: uuid.UUID, session: SessionDep, current_user: CurrentUser) -> Any:
+    """
+    Get a specific user by id.
+    """
+    user = session.get(User, user_id)
+    if user == current_user:
+        return user
+    if not current_user.is_superuser:
+        raise HTTPException(
+            status_code=403,
+            detail="用户没有足够的权限",
+        )
+    if user is None:
+        raise HTTPException(status_code=404, detail="用户不存在")
+    return user
+
+
+@router.patch(
+    "/{user_id}",
+    dependencies=[Depends(get_current_active_superuser)],
+    response_model=UserPublic,
+)
+def update_user(
+    *,
+    session: SessionDep,
+    user_id: uuid.UUID,
+    user_in: UserUpdate,
+) -> Any:
+    """
+    Update a user.
+    """
+
+    db_user = session.get(User, user_id)
+    if not db_user:
+        raise HTTPException(
+            status_code=404,
+            detail="该用户ID在系统中不存在",
+        )
+    if user_in.email:
+        existing_user = user_crud.get_user_by_email(session=session, email=user_in.email)
+        if existing_user and existing_user.id != user_id:
+            raise HTTPException(status_code=409, detail="该邮箱的用户已存在")
+
+    db_user = user_crud.update_user(session=session, db_user=db_user, user_in=user_in)
+    return db_user
+
+
+@router.delete("/{user_id}", dependencies=[Depends(get_current_active_superuser)])
+def delete_user(session: SessionDep, current_user: CurrentUser, user_id: uuid.UUID) -> Message:
+    """
+    Delete a user.
+    """
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="用户不存在")
+    if user == current_user:
+        raise HTTPException(status_code=403, detail="超级用户不允许自行删除。")
+    # statement = delete(Item).where(col(Item.owner_id) == user_id)
+    # session.exec(statement)
+    session.delete(user)
+    session.commit()
+    return Message(message="用户已成功删除")
